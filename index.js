1cbf6de19f4c718cdf50e5fa1060722e443f1b66
Matthew Westbrook <horrorvacui@tutanota.com>
2016-11-17 00:58:58 -0600

    Add db config and shortenUrl logic

---
+++ index.js
@@ -0,0 +1,10 @@
+let express          = require('express'),
+  app                = express(),
+  port               = process.env.PORT || 5000,
+  urlShortener      = require('./lib/url-shortener.js');
+
+app.get('/new/:url', urlShortener);
+
+let listener = app.listen(port, function () {
+  console.log('your app is listening on port ' + listener.address().port);
+});
