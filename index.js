7c8ec134b789843b539d2bdbf6d5fef8bcc4174c
Matthew Westbrook <horrorvacui@tutanota.com>
2016-11-17 20:39:34 -0600

    Add pug

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ let express          = require('express'),
   port               = process.env.PORT || 5000,
   urlShortener      = require('./lib/url-shortener.js');
 
+app.set('web engine', 'pug');
+
 app.get('/new/:url', urlShortener);
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 let listener = app.listen(port, function () {
   console.log('your app is listening on port ' + listener.address().port);
