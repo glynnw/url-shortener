df987c553d53ce5e336c8a79f4322934b66ea8c8
Matthew Westbrook <horrorvacui@tutanota.com>
2016-11-19 01:07:24 -0600

    Fixes after linting code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-"use strict";
-let express          = require('express'),
-  app                = express(),
-  port               = process.env.PORT || 5000,
-  shortUrlController = require('./lib/short-url-controller.js'),
-  stubController     = require('./lib/stub-controller.js');
+const express = require('express');
+const shortUrlController = require('./lib/short-url-controller.js');
+const stubController = require('./lib/stub-controller.js');
+const logger = require('winston');
+
+const port = process.env.PORT || 5000;
+const app = express();
 
 app.set('view engine', 'pug');
-
 app.use('/', stubController);
 app.use('/new', shortUrlController);
 
-let listener = app.listen(port, function () {
-  console.log('your app is listening on port ' + listener.address().port);
+const listener = app.listen(port, () => {
+  logger.info(`Your app is listening on port ${listener.address().port}`);
 });
